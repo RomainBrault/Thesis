043bef40e4a5b7092142df754e25a31cf7912c91
RomainBrault <ro.brault@gmail.com>
2016-10-16 20:29:06 +0200

    Sync with Overleaf

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 set -e
 
+git pull overleaf master
 rm -rf build || true
 mkdir build
 cd build
@@ -7,3 +8,6 @@ cmake ..
 make -j8
 mv ThesisRomainBrault.pdf ..
 cd ..
+git add -A
+git commit -m"Sync with Overleaf"
+git push both

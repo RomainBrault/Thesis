8544ea3fe5d2a2bb9a0d94835ed58e127f136812
RomainBrault <ro.brault@gmail.com>
2016-10-23 17:49:28 +0200

    Sync with Overleaf

--- update.sh
+++ update.sh
@@ -5,3 +5,10 @@ git pull github master
 git add -A
 git commit -m"Sync with Overleaf"
 git push both
+
+
+git pull overleaf master
+git pull github master
+git add -A
+git commit -m"Sync with Overleaf"
+git push both

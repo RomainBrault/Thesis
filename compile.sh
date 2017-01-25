c67f599a0c90f98ffc372a3339e511df5d5ae252
RomainBrault <ro.brault@gmail.com>
2017-01-25 18:48:27 +0100

    Sync with Overleaf

--- compile.sh
+++ compile.sh
@@ -1,8 +1,37 @@
+#!/bin/sh
+
 set -e
 
-git pull github master
-rm -rf build || true
-mkdir build
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+UPDATE=false
+FORCE=false
+
+while getopts "h?uf" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    u)  UPDATE=true
+        ;;
+    f)  FORCE=true
+        ;;
+    esac
+done
+
+if [[ ${UPDATE} = true ]]; then
+    git pull github master
+fi
+if [[ ${FORCE} = true ]]; then
+    rm -rf build || true
+fi
+mkdir -p build
 cd build
-cmake ..
-make -j1
+if [[ ${FORCE} = true ]]; then
+    cmake ..
+    make all
+else
+    make ThesisRomainBrault
+fi
+cd ..
